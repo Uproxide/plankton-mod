7b66850c4be14dc305d2cd7e456334819222e1e0
Uproxide <153382689+Uproxide@users.noreply.github.com>
2024-04-22 15:08:03 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,21 @@
 using namespace geode::prelude;
 
 #include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
+class $modify(MenuLayer) {
 	bool init() {
 		if (!MenuLayer::init()) {
 			return false;
 		}
 
-		auto planktinButton = CCMenuItemSpriteExtra::create(
-			CCSprite::create("GJ_likeBtn_001.png"),
-			this,
-			menu_selector(MyMenuLayer::onPlanktinButton)
-		);
-
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(planktinButton);
-
-		planktinButton->setID("planktin-button"_spr);
-
-		menu->updateLayout();
-
-		return true;
-	}
-
-	void onPlanktinButton(CCObject*) {
 		auto plank = CCSprite::create("plankton"_spr);
 		auto text = CCLabelBMFont::create("All hail plankerton :fire:", "bigFont.fnt");
 
 		this->addChild(plank);
 		this->addChild(text);
 
-		plank->setPosition(100, 80);
-		text->setPosition(100, 75);
+		plank->setPosition(100f, 80f);
+		text->setPosition(100f, 75f);
+
+		return true;
 	}
 };
